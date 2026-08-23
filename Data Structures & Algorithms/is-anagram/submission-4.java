c9a3443d83e92c93cc773d76a0f2f70a12e62661
Conner McGill <43885110+ConnerMcGill@users.noreply.github.com>
2026-08-23 15:06:47 +0100

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        char[] result = new char[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            result[s.charAt(i) - 'a']++;
+            result[t.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] != 0) return false;
+        }
+        return true;
+    }
+}
