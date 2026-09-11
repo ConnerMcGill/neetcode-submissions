64a69fc59426ef782413ab678514db107d557916
Conner McGill <43885110+ConnerMcGill@users.noreply.github.com>
2026-09-11 20:56:25 +0100

    Add: anagram-groups - submission-13

---
+++ Data Structures & Algorithms/anagram-groups/submission-13.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> hashmap = new HashMap<>();
+
+        for (String s : strs) {
+            char[] count = new char[26];
+
+            for (int i = 0; i < s.length(); i++) {
+                count[s.charAt(i) - 'a']++;
+            }
+
+            String key = Arrays.toString(count);
+            hashmap.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+        }
+
+        return new ArrayList<>(hashmap.values());
+    }
+}
